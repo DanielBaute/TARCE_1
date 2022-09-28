2b2e784370988ca2655fa708f95b536ff8f52cb3
alexis <dadialop@gmail.com>
2022-09-28 19:11:48 +0100

    v2localA

--- routes/mivista.js
+++ routes/mivista.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('<h1>Esto es una nueva vista</h1>');
+  res.render('mivista');
 });
 
 module.exports = router;
