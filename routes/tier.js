a1c29a356c07a240764e6eb8233b48607489b849
Dani <danibaute99@gmail.com>
2022-11-27 18:05:54 +0000

    'Definitivo1'

--- routes/tier.js
+++ routes/tier.js
@@ -1,9 +1,30 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('tier', { title: 'Problema 1'});
-});
+
+
+router.get('/', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM lol_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('tier', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+
 
 module.exports = router;
